2113a881ef87e5d4e4b55dca80d27fcadb2f3f24
ocnarb <dreroips@gmail.com>
2021-08-11 13:20:20 -0300

    Commit

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,14 +1,22 @@
-import styles from '../styles.module.scss'
+
+import { SignInButton } from '../SignInButton';
+
+
+import Image from 'next/image'
+import Logo from '../../../public/images/logo.svg'
+
+import styles from './styles.module.scss'
 
 export function Header() {
   return (
     <header className={styles.Container}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="AnbraNews" />
+        <Image src={Logo} alt="AnbraNews" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,46 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import styles from './home.module.scss'
+
+import Image from 'next/image';
+import img from '../../public/images/avatar.svg'
+import { SubscribeButton } from '../Components/SubscribeButton';
+import stripe from 'stripe';
 
 export default function Home() {
+
   return (
     <>
       <Head>
         <title>Anbra News</title>
       </Head>
-      <h1>
-        Hello
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Seja bem vindo(a)</span>
+          <h1>Artigos e tutoriais sobre <span>programação.</span></h1>
+          <p>Tenha acesso a todas publicações <br />
+            <span>por $1.99 mês</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <Image src={img} alt="Girl" />
+      </main>
+
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JMz0eLahqNxUOrfvfVYTFrV', {
+    expand: ['product']
+  })
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  };
+  return {
+    props: {
+      nome: 'Ocnarb'
+    }
+  }
 }
